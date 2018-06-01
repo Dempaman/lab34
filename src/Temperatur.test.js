a91f922b52bf1e28d92f70e2c3c5141bda934780
Dennis Björk <dennis.bjork87@gmail.com>
2018-06-01 12:00:54 +0200

    jest fix

--- src/Temperatur.test.js
+++ src/Temperatur.test.js
@@ -2,6 +2,6 @@ import {toFahrenheit} from './Temperatur.js'
 
 describe('converte to fahrenheit', ()=>{
   it('renders celsius to fahrenheit', ()=>{
-    expect(toFahrenheit('0')).toBe(32)
+    expect(toFahrenheit(0)).toBe(32)
   });
 })
