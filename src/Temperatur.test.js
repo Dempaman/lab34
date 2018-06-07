1c9e531184631fbdef5a304092ed1b0c779b85d3
Dennis Björk <dennis.bjork87@gmail.com>
2018-06-07 10:42:42 +0200

    fix

--- src/Temperatur.test.js
+++ src/Temperatur.test.js
@@ -1,7 +1,10 @@
 import {toFahrenheit} from './Temperatur.js'
 
 describe('converte to fahrenheit', ()=>{
-  it('renders celsius to fahrenheit', ()=>{
+  it('#1 renders celsius to fahrenheit', ()=>{
     expect(toFahrenheit(0)).toBe(32)
+  });
+  it('#2 convert string to number', ()=>{
+    expect(toFahrenheit('32')).toBe(89.6)
   });
 })
